469d8a84ada00c4055b0084456d3f387f6d75e65
aam198 <aam198@psu.edu>
2022-03-14 09:03:49 -0400

    Linting

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
       {jobs.map(job => {
-        return <Job key={job.uniqueId} job={job} />
+        return <Job key={job.id} job={job} />
       })}
     <ScrollToTop showUnder={200}>
          <span>
